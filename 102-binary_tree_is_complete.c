eb73c6cc8f457b85f03deeb40187b71f59e9312e
MoodyEG <moodyeg2011@yahoo.com>
2024-03-27 07:18:41 -0700

    Fix

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -42,7 +42,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	size_t size;
 
 	if (tree == NULL)
-		return (1);
+		return (0);
 	size = binary_tree_size(tree);
 	return (is_complete(tree, 0, size));
 }
